e797fbea12316358ab0b7b6d3a855f8c9f9c2950
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-13 18:59:04 -0700

    Adds initial boilerplate for company detail view.

--- frontend/src/components/CompanyDetail.vue
+++ frontend/src/components/CompanyDetail.vue
@@ -1,7 +1,36 @@
 <script setup>
-// todo
+import { onMounted } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+//import {storeToRefs } from 'pinia'
+import { useCompany } from '@/stores/company'
+
+const companyStore = useCompany()
+
+onMounted(() => {
+  companyStore.fetchCompany(this.$route.query.slug)
+})
+
 </script>
 
 <template>
-<!-- todo -->
+<div>
+  <h1>
+    <!-- <span>Company: {{ companiesStore.company?.name }}</span><br /> -->
+    <!-- <small>{{ companiesStore.company?.slug }}</small> -->
+  </h1>
+  <!-- <RouterLink :to="{ name: 'companies-list' }"> -->
+    <!-- < Companies -->
+  <!-- </RouterLink> -->
+
+  <code>
+    {{ this.$route.query }}
+  </code>
+  <p>
+  <!-- {{ companiesStore?.company.description }} -->
+  </p>
+</div>
 </template>
+
+<style scoped>
+/* todo: styles */
+</style>
--- frontend/src/stores/company.js
+++ frontend/src/stores/company.js
@@ -1,11 +1,45 @@
-  // getSlug(state) {
-  //   if(!state?.slug) {
-  //     state.slug = state.title
-  //       .toLowerCase()
-  //       .trim()
-  //       .replace(/[^a-z0-9]+/g, '-')
-  //     ;
-  //   }
+import { defineStore } from 'pinia'
+import { supabase } from '@/lib/supabaseClient'
 
-  //   return state.slug;
-  // },
+export const useCompany = defineStore('company', {
+  // state
+  state: () => ({
+    company: [],
+    // company: {
+    //   id: 0,
+    //   slug: '',
+    //   //
+    // },
+  }),
+  getters: {
+    //
+  },
+  actions: {
+    async fetchCompany(slug) {
+      const { data, error } = await supabase.from('companies').select('id, slug').eq('slug', slug)
+      //const { data, error } = await supabase.from('companies').select('where id = ') // todo: this probably needs to use filters
+
+      if (error) {
+        console.error(error)
+        throw error
+      }
+      this.companies = data
+    },
+    updateFilters(newFilters) {
+      const { search, ...rest } = newFilters
+      this.filters = { search: search ?? '', ...rest }
+      // fetchCompanies()
+    },
+    // addCompany(attributes) {
+    //   this.companies.push({
+    //     name: attributes?.name,
+    //     slug: attributes?.slug,
+    //     description: attributes?.description,
+    //     website: attributes?.website,
+    //     job_board_url: attributes?.job_board_url,
+    //     logo_url: attributes?.logo_url,
+    //     industry: attributes?.industry,
+    //   })
+    // },
+  },
+})
