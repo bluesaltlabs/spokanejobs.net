6998a85502ef2d5e985d090789ea64a48303dff3
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-16 09:48:38 -0700

    Adds json syntax stuff I found somewhere.

--- scraper/internal/models/scraped_job.go
+++ scraper/internal/models/scraped_job.go
@@ -1,11 +1,11 @@
 package models
 
 type ScrapedJob struct {
-	JobId string
-	Title string
-	Description string
-	City string
-	State string
-	Url string
+	JobId 			string `json:"job_id"`
+	Title 			string `json:"title"`
+	Description	string `json:"description"`
+	City 				string `json:"city"`
+	State 			string `json:"state"`
+	Url 				string `json:"url"`
 	// todo: needs more attributes, need to state which ones are optional, etc.
 }
