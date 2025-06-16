a6a7a1440d6e9f01e6160268af6777002a12432a
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-16 12:23:40 -0700

    Updates scraper to use log. Updates how job urls are retrieved.

--- scraper/internal/scrapers/openeye/openeye_scraper.go
+++ scraper/internal/scrapers/openeye/openeye_scraper.go
@@ -2,6 +2,7 @@ package openeye
 
 import (
 	"fmt"
+	"log"
 	"encoding/json"
 	"os"
 	"strings"
@@ -38,13 +39,13 @@ func ScrapeJobs() []models.ScrapedJob {
   c.OnRequest(func(r *colly.Request) {
     r.Headers.Set("Accept-Language", "en-US")
     r.Headers.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.10 Safari/605.1.1")
-    fmt.Printf("Colly is visiting: %s\n", r.URL)
+    log.Printf("visiting: %s\n", r.URL.String()) // ?
   })
 
 
   // Error handling
   c.OnError(func(r *colly.Response, e error) {
-    fmt.Printf("Error while scraping: %s\n", e.Error())
+    log.Printf("Error while scraping: %s\n", e.Error())
   })
 
 
@@ -54,13 +55,13 @@ func ScrapeJobs() []models.ScrapedJob {
   	// todo: consider making a map of jobs here.
     // Create the scraped_job struct instance
     job := models.ScrapedJob{}
+    url := h.Attr("href")
     selection := h.DOM
 
     // job detail url prefix (todo: extract JobId from the url)
     // https://job-boards.greenhouse.io/openeye/jobs/<JobId>
 
     // Retrieve attributes available
-    url, _ := selection.Attr("href")
     title := trimSpaces(selection.Find("p.body.body--medium").Text())
     location := trimSpaces(selection.Find("p.body.body__secondary.body--metadata").Text())
     job.Url = url
