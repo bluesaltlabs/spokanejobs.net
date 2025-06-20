fd1d496e6a11e4183b170a1e908ac19f1020279b
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-06-19 23:57:26 -0700

    Adds job board button to company detail page. Updates styling.

--- frontend/src/components/CompanyDetail.vue
+++ frontend/src/components/CompanyDetail.vue
@@ -17,12 +17,16 @@ const company = computed(() => companyStore.company)
 <div>
   <!-- todo: back to companies grid header -->
   <div class="back-button">
-    <router-link class="back-button-link" to="/companies"> < Companies</router-link>
+    <router-link class="back-button-link" to="/companies">🏢 All Companies</router-link>
   </div>
 </div>
   <div v-if="company">
-    <h1>{{ company.name }}</h1>
-    <p v-if="company.website"><a :href="company.website" target="_blank">{{ company.website }}</a></p>
+    <h1 class="company-name">{{ company.name }}</h1>
+    <div class="company-links-container">
+      <span v-if="company.website"><a :href="company.website" target="_blank" :alt="company.website">🌐 Website</a></span>
+      <span v-if="company.job_board_url"><a :href="company.job_board_url" target="_blank" :alt="company.job_board_url">💼 Jobs</a></span>
+    </div>
+
     <div class="spacer"></div>
     <img v-if="company?.logo_url" :src="company.logo_url" :alt="company.name + ' Logo'" class="company-logo" />
 
@@ -37,6 +41,12 @@ const company = computed(() => companyStore.company)
 </template>
 
 <style scoped>
+
+.company-name {
+  margin-top: 25px;
+  margin-bottom: 15px;
+  font-size: 3.5rem;
+}
 
 img.company-logo {
   max-height: 425px;
@@ -62,6 +72,29 @@ img.company-logo {
 
 .spacer {
   height: 30px;
+}
+
+.company-links-container {
+  margin-top: 15px;
+  margin-bottom: 15px;
+
+
+  a {
+    border-radius: var(--border-radius-small);
+    padding: 5px 10px;
+    margin: 5px 8px;
+    background: var(--color-primary-700);
+    color: var(--color-text);
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.2rem;
+
+    &:hover {
+      background: var(--color-primary-500);
+      color: var(--color-white);
+    }
+
+  }
 }
 
 </style>
