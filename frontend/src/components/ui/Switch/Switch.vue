52980958eb02ea334bc570aadb8b6badb2c9d517
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-08 17:19:13 -0700

    Updates switch component styling. Adds ability to include icon in toggle.

--- frontend/src/components/ui/Switch/Switch.vue
+++ frontend/src/components/ui/Switch/Switch.vue
@@ -6,7 +6,11 @@
       :disabled="disabled"
       @change="$emit('update:modelValue', $event.target.checked)"
     />
-    <span class="ui-switch__slider"></span>
+    <span class="ui-switch__slider">
+      <span class="ui-switch__toggle">
+        <slot />
+      </span>
+    </span>
     <span v-if="label" class="ui-switch__label">{{ label }}</span>
   </label>
 </template>
@@ -29,33 +33,52 @@ defineProps({
   display: none;
 }
 .ui-switch__slider {
-  width: 2em;
-  height: 1em;
-  background: #d1d5db;
+  width: 3.5em;
+  height: 2em;
+  background: var(--color-gray-300);
   border-radius: 1em;
   position: relative;
   transition: background 0.2s;
   margin-right: 0.5em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 .ui-switch input:checked + .ui-switch__slider {
-  background: #2563eb;
+  background: var(--color-primary-600);
 }
-.ui-switch__slider::after {
-  content: '';
+.ui-switch__toggle {
   position: absolute;
-  left: 0.2em;
-  top: 0.2em;
-  width: 0.6em;
-  height: 0.6em;
-  background: #fff;
+  left: 0.25em;
+  top: 0.25em;
+  width: 1.5em;
+  height: 1.5em;
+  background: var(--color-white);
   border-radius: 50%;
   transition: left 0.2s;
+  box-shadow: var(--color-shadow);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.ui-switch input:checked + .ui-switch__slider::after {
-  left: 1.2em;
+.ui-switch input:checked + .ui-switch__slider .ui-switch__toggle {
+  left: 1.75em;
 }
+
+/* Style SVG icons in the toggle circle */
+.ui-switch__toggle :deep(svg) {
+  width: 0.75em;
+  height: 0.75em;
+  color: var(--color-gray-600);
+  transition: color 0.2s;
+}
+
+.ui-switch input:checked + .ui-switch__slider .ui-switch__toggle :deep(svg) {
+  color: var(--color-primary-600);
+}
+
 .ui-switch__label {
   font-size: 1rem;
-  color: #111;
+  color: var(--color-text);
 }
-</style> 
+</style>
